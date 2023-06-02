b987fe00fd7dbbc814f55e7c28d6f0492a61fd93
meganrm <meganrm@gmail.com>
2023-06-02 10:39:23 -0700

    Bump to 0.1.0-dev-1

--- nbsv/_version.py
+++ nbsv/_version.py
@@ -5,7 +5,7 @@
 # Distributed under the terms of the Modified BSD License.
 
 
-version_info = (0, 1, 0, "dev-0")
+version_info = (0, 1, 0, "dev-1")
 public_version = version_info[0:3]
 extra = version_info[3]
 __version__ = f'".".join(map(str, (public_version,)))-{extra}'
