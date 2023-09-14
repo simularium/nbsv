a9c9c9dcfa04e53b359b5e9b7296e20211fe0fea
ascibisz <alli.scibisz@alleninstitute.org>
2023-09-14 14:18:09 -0700

    update test so it'll pass

--- nbsv/tests/test_example.py
+++ nbsv/tests/test_example.py
@@ -4,11 +4,25 @@
 # Copyright (c) Megan Riel-Mehan.
 # Distributed under the terms of the Modified BSD License.
 
-import pytest
-import ipywidgets
+import numpy as np
+from simulariumio import TrajectoryData, MetaData, AgentData
 from ..viewer import ViewerWidget
 
 
-def test_example_creation_blank():
-    w = ViewerWidget()
+def test_example_creation():
+    trajectory = TrajectoryData(
+        meta_data=MetaData(
+            box_size=np.array([10.0, 10.0, 10.0]),
+        ),
+        agent_data=AgentData(
+            times=[0.0],
+            n_agents=[1],
+            viz_types=[[1000.0]],
+            unique_ids=[[1]],
+            types=[['A']],
+            positions=[[[0, 0, 0]]],
+            radii=[[5]]
+        )
+    )
+    w = ViewerWidget(trajectory)
     assert w.height == 300
