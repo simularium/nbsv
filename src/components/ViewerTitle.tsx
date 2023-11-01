39f100b78a3c3e3cddaf750685d4150acb62faa4
Joe Heffernan <interim17@gmail.com>
2023-11-01 10:59:57 -0700

    title styling and icon

--- src/components/ViewerTitle.tsx
+++ src/components/ViewerTitle.tsx
@@ -1,17 +1,30 @@
 import * as React from 'react';
 
 import '../../css/viewerTitle.css';
+import { Info } from './Icons';
 
 interface ViewerTitleProps {
   title: string;
+  trajectoryInfo?: string;
 }
 
 const ViewerTitle: React.FunctionComponent<ViewerTitleProps> = (
   props: ViewerTitleProps
 ): JSX.Element => {
+  const renderInfoCard = () => <div> INFO CARD </div>;
+
   return (
     <div className="title-container">
-      <div className="title"> {props.title} </div>
+      <div className="title">
+        {' '}
+        {props.title}{' '}
+        {props.trajectoryInfo ? (
+          <div className="info-button" onClick={renderInfoCard}>
+            {' '}
+            {Info}{' '}
+          </div>
+        ) : null}
+      </div>
     </div>
   );
 };
