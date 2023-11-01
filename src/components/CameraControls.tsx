91f3584cf3a0d5d8608fe0398bbcffef0fced42f
Joe Heffernan <interim17@gmail.com>
2023-11-01 15:34:37 -0700

    camera controls component

---
+++ src/components/CameraControls.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+
+import { Button, Tooltip } from 'antd';
+import { SimulariumController } from '@aics/simularium-viewer';
+import { Reset, ZoomIn, ZoomOut } from './Icons';
+
+import '../../css/cameraControls.css';
+
+interface CameraControlsProps {
+  title: string;
+  controller: SimulariumController;
+}
+
+const TOOLTIP_COLOR = '#3B3649';
+
+const CameraControls: React.FunctionComponent<CameraControlsProps> = (
+  props: CameraControlsProps
+): JSX.Element => {
+  return (
+    <div className="cc-container">
+      <Tooltip
+        placement="left"
+        title="Zoom in ( &uarr; )"
+        color={TOOLTIP_COLOR}
+      >
+        <Button
+          className="btn"
+          icon={ZoomIn}
+          onClick={props.controller.zoomIn}
+        ></Button>
+      </Tooltip>
+      <Tooltip
+        placement="left"
+        title="Zoom out ( &darr; )"
+        color={TOOLTIP_COLOR}
+      >
+        <Button
+          className="btn"
+          icon={ZoomOut}
+          onClick={props.controller.zoomOut}
+        ></Button>
+      </Tooltip>
+      <Tooltip placement="left" title="Home view (H)" color={TOOLTIP_COLOR}>
+        <Button
+          className="btn"
+          icon={Reset}
+          onClick={props.controller.resetCamera}
+        ></Button>
+      </Tooltip>
+    </div>
+  );
+};
+
+export default CameraControls;
