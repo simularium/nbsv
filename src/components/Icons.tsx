31baf3fd3cefe4a6965692894720b00a7f909e66
Joe Heffernan <interim17@gmail.com>
2023-11-16 11:50:17 -0800

    Feature/camera controls (#3)
    * create icons file, add camera control icons
    
    * camera controls component
    
    * camera controls css
    
    * render camera controls in viewer
    
    * grab traj data and pass title down
    
    * remove debug loggerlevel
    
    * bump viewer add colorChanges to SSI
    
    * case sensitivity build failure fix attempt
    
    * delete package-lock
    
    * install antd icons with yarn
    
    * restore package lock to test build failure
    
    * removing package lock to test windows build
    
    * loggerlevel set to off
    
    * remove title data

---
+++ src/components/Icons.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { PlusOutlined, MinusOutlined, HomeOutlined } from '@ant-design/icons';
+
+export const Reset = <HomeOutlined />;
+export const ZoomIn = <PlusOutlined />;
+export const ZoomOut = <MinusOutlined />;
+
+export default {
+  Reset,
+  ZoomIn,
+  ZoomOut,
+};
