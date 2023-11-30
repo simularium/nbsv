324547fd65e63ec5e2bc44c0628ebef659c6130e
Joe Heffernan <interim17@gmail.com>
2023-11-30 15:11:28 -0800

    handle traj info with viewer methods

--- src/Viewer.tsx
+++ src/Viewer.tsx
@@ -2,12 +2,12 @@ import SimulariumViewer, {
   RenderStyle,
   SimulariumController,
 } from '@aics/simularium-viewer';
-import React from 'react';
+import React, { useState } from 'react';
 
 import { WidgetModel } from '@jupyter-widgets/base';
 import CameraControls from './components/CameraControls';
 import ViewerTitle from './components/ViewerTitle';
-import { ModelInfo } from './constants';
+import { ModelInfo, agentColors } from './constants';
 
 import '../css/viewer.css';
 
@@ -19,33 +19,18 @@ export interface WidgetProps {
   controller: SimulariumController;
   height: number;
   width: number;
-  modelInfo: ModelInfo;
 }
 
-const agentColors = [
-  '#fee34d',
-  '#f7b232',
-  '#bf5736',
-  '#94a7fc',
-  '#ce8ec9',
-  '#58606c',
-  '#0ba345',
-  '#9267cb',
-  '#81dbe6',
-  '#bd7800',
-  '#bbbb99',
-  '#5b79f0',
-  '#89a500',
-  '#da8692',
-  '#418463',
-  '#9f516c',
-  '#00aabf',
-];
-
 function ViewerWidget(props: WidgetProps): JSX.Element {
+  const [modelInfo, setModelInfo] = useState<ModelInfo>({});
+
+  const handleTrajectoryData = (data: any) => {
+    setModelInfo(data.modelInfo);
+  };
+
   return (
     <div>
-      <ViewerTitle modelInfo={props.modelInfo} />
+      <ViewerTitle modelInfo={modelInfo} />
       <div className="viewer-container">
         <SimulariumViewer
           renderStyle={RenderStyle.WEBGL2_PREFERRED}
@@ -57,7 +42,7 @@ function ViewerWidget(props: WidgetProps): JSX.Element {
           simulariumController={props.controller}
           onJsonDataArrived={console.log}
           showCameraControls={false}
-          onTrajectoryFileInfoChanged={console.log}
+          onTrajectoryFileInfoChanged={handleTrajectoryData}
           selectionStateInfo={{
             highlightedAgents: [],
             hiddenAgents: [],
