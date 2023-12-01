2c238ab8d4e83d34f28a0ae2f9b1553007a1bc9c
Joe Heffernan <interim17@gmail.com>
2023-12-01 14:48:42 -0800

    receive new props, display either title

--- src/components/ViewerTitle.tsx
+++ src/components/ViewerTitle.tsx
@@ -4,18 +4,22 @@ import { Info } from './Icons';
 import { ModelInfo } from '../constants';
 
 import '../../css/viewer_title.css';
-interface ViewerTitleProps {
-  modelInfo: ModelInfo;
+
+interface ViewerTitleProps extends ModelInfo {
+  trajectoryTitle?: string;
 }
 
 const ViewerTitle: React.FunctionComponent<ViewerTitleProps> = (
   props: ViewerTitleProps
 ): JSX.Element => {
+  const { title, trajectoryTitle } = props;
+
   const hasMetaData = (): boolean => {
     for (const key in props) {
+      // Check if any key other than 'title' has a value
       if (
         key !== 'title' &&
-        props.modelInfo[key as keyof ModelInfo] !== undefined
+        props[key as keyof ViewerTitleProps] !== undefined
       ) {
         return true;
       }
@@ -26,7 +30,7 @@ const ViewerTitle: React.FunctionComponent<ViewerTitleProps> = (
   return (
     <div className="title-container">
       <div className="title">
-        {props.modelInfo.title}
+        {trajectoryTitle || title}
         {/* TODO clicking Info icon will render meta data panel */}
         {hasMetaData() ? <div className="info-button"> {Info} </div> : null}
       </div>
