2813829286412fb86dc012156bb43706b6858288
Joe Heffernan <interim17@gmail.com>
2023-12-20 16:29:04 -0800

    create side panel component

---
+++ src/components/SidePanel.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+
+import '../../css/side_panel.css';
+
+import { Card } from 'antd';
+
+// TODO placeholder, side panel will receive many props when rendering checkboxes
+interface SidePanelProps {}
+
+const SidePanel: React.FunctionComponent<SidePanelProps> = (
+  props: SidePanelProps
+): JSX.Element => {
+  return (
+    <div className="sp-container">
+      <Card className="title-card" bordered={false}>
+        {'Agents'}
+      </Card>
+    </div>
+  );
+};
+
+export default SidePanel;
