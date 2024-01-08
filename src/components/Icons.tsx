3a58597336ba9449782e37b9f93aa620e78c0781
Joe Heffernan <interim17@gmail.com>
2024-01-08 14:12:07 -0800

    add initial close icon for meta data panel

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -4,13 +4,18 @@ import {
   MinusOutlined,
   HomeOutlined,
   InfoCircleFilled,
+  PlusCircleOutlined,
 } from '@ant-design/icons';
 
 export const Reset = <HomeOutlined />;
 export const ZoomIn = <PlusOutlined />;
 export const ZoomOut = <MinusOutlined />;
 export const Info = <InfoCircleFilled />;
-
+export const Close = (
+  <PlusCircleOutlined
+    style={{ transform: 'rotate(45deg)', fontSize: '24px' }}
+  />
+);
 export default {
   Reset,
   ZoomIn,
