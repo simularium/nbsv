23b3a5aedf618c9b153aaafa837bae88a6d9b95f
Joe Heffernan <interim17@gmail.com>
2024-01-17 12:50:33 -0800

    new icons for checkbox tree

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -4,12 +4,20 @@ import {
   MinusOutlined,
   HomeOutlined,
   InfoCircleFilled,
+  StarOutlined,
+  StarFilled,
+  CaretRightFilled,
+  CaretDownFilled,
 } from '@ant-design/icons';
 
 export const Reset = <HomeOutlined />;
 export const ZoomIn = <PlusOutlined />;
 export const ZoomOut = <MinusOutlined />;
 export const Info = <InfoCircleFilled />;
+export const Star = <StarOutlined />;
+export const StarFilledIcon = <StarFilled />;
+export const CaretRight = <CaretRightFilled />;
+export const CaretDown = <CaretDownFilled />;
 
 export default {
   Reset,
