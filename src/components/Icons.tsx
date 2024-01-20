780d2df226135bf3c7bb9cc22a9485b70bff1d7e
Joe Heffernan <interim17@gmail.com>
2024-01-19 18:00:40 -0800

    add icons for hidden/highlight interim states

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -5,17 +5,27 @@ import {
   HomeOutlined,
   InfoCircleFilled,
   StarOutlined,
+  StarTwoTone,
   StarFilled,
   CaretRightFilled,
   CaretDownFilled,
+  CheckSquareFilled,
+  MinusSquareFilled,
+  CloseSquareFilled,
 } from '@ant-design/icons';
 
 export const Reset = <HomeOutlined />;
 export const ZoomIn = <PlusOutlined />;
 export const ZoomOut = <MinusOutlined />;
 export const Info = <InfoCircleFilled />;
-export const Star = <StarOutlined />;
-export const StarFilledIcon = <StarFilled />;
+export const NoHighlightStar = <StarOutlined />;
+export const HighlightStar = <StarFilled />;
+export const IndeterminateHighlightStar = (
+  <StarTwoTone twoToneColor="#b59ff6" />
+);
+export const VisibleCheck = <CheckSquareFilled />;
+export const IndeterminateCheck = <MinusSquareFilled />;
+export const HiddenCheck = <CloseSquareFilled />;
 export const CaretRight = <CaretRightFilled />;
 export const CaretDown = <CaretDownFilled />;
 
