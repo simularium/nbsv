fec818e70548415e9b388107e7b06eb808900750
Joe Heffernan <interim17@gmail.com>
2024-01-20 13:04:19 -0800

    add playback interim icons

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -4,12 +4,22 @@ import {
   MinusOutlined,
   HomeOutlined,
   InfoCircleFilled,
+  ItalicOutlined,
+  StepBackwardOutlined,
+  StepForwardOutlined,
+  CaretRightOutlined,
+  PauseOutlined,
 } from '@ant-design/icons';
 
 export const Reset = <HomeOutlined />;
 export const ZoomIn = <PlusOutlined />;
 export const ZoomOut = <MinusOutlined />;
 export const Info = <InfoCircleFilled />;
+export const ScaleBarIcon = <ItalicOutlined />;
+export const FrameBack = <StepBackwardOutlined />;
+export const FrameForward = <StepForwardOutlined />;
+export const Pause = <PauseOutlined />;
+export const Play = <CaretRightOutlined />;
 
 export default {
   Reset,
