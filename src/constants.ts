5c5702a2524222b4b1164dcfdbe2a60c66c95a5d
Joe Heffernan <interim17@gmail.com>
2024-01-19 18:00:49 -0800

    add constants for display states

--- src/constants.ts
+++ src/constants.ts
@@ -17,3 +17,38 @@ export const agentColors = [
   '#9f516c',
   '#00aabf',
 ];
+
+export enum HiddenOrHighlightedState {
+  Active,
+  Inactive,
+  Indeterminate,
+}
+
+// maps names/display states for viewer viz status (hidden/highligted or not)
+// called VisiblitySelectionMap in website repo
+export interface ViewerVisibilityMap {
+  [key: string]: string[];
+}
+
+// maps agent/subagent name to current ui viz status
+// (hidden, visible, indeterminate) - used for visibility and highlighting
+export interface UIVisibilityMap {
+  [key: string]: HiddenOrHighlightedState;
+}
+
+//todo get from viewer
+export interface DisplayStateEntry {
+  name: string;
+  id: string;
+  color: string;
+}
+
+export enum DisplayAction {
+  Hide = 'hidden',
+  Highlight = 'highlight',
+}
+
+export interface ViewerVisibilityStates {
+  hidden: ViewerVisibilityMap;
+  highlight: ViewerVisibilityMap;
+}
