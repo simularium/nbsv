82c5655cc80e0f7e03e7d5242c92e316ea03148f
Joe Heffernan <interim17@gmail.com>
2024-01-21 10:26:13 -0800

    import scale bar icon

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
+import miniSVGDataURI from 'mini-svg-data-uri';
 import {
   PlusOutlined,
   MinusOutlined,
   HomeOutlined,
   InfoCircleFilled,
-  ItalicOutlined,
   StepBackwardOutlined,
   StepForwardOutlined,
   CaretRightOutlined,
   PauseOutlined,
 } from '@ant-design/icons';
+import scaleBarSVG from '../../assets/scale-bar.svg';
 
 export const Reset = <HomeOutlined />;
 export const ZoomIn = <PlusOutlined />;
 export const ZoomOut = <MinusOutlined />;
 export const Info = <InfoCircleFilled />;
-export const ScaleBarIcon = <ItalicOutlined />;
 export const FrameBack = <StepBackwardOutlined />;
 export const FrameForward = <StepForwardOutlined />;
 export const Pause = <PauseOutlined />;
 export const Play = <CaretRightOutlined />;
+export const ScaleBarIcon = (
+  <img src={miniSVGDataURI(scaleBarSVG)} style={{ width: '60px' }} />
+);
 
 export default {
   Reset,
