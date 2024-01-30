56d5921016971d0d0f6bdab69b8173382a8d80f2
Joe Heffernan <interim17@gmail.com>
2024-01-30 07:37:41 -0800

    declare icons in svgs.tsx

---
+++ src/assets/svgs.tsx
@@ -0,0 +1,101 @@
+import React from 'react';
+
+export const ScaleBarIconTag = (
+  <svg
+    enable-background="new 0 0 150 75.3"
+    viewBox="0 0 150 75.3"
+    xmlns="http://www.w3.org/2000/svg"
+    width="60px"
+  >
+    <linearGradient
+      id="a"
+      gradientTransform="matrix(1 0 0 -1 0 76)"
+      gradientUnits="userSpaceOnUse"
+      x1=".3035"
+      x2="149.6965"
+      y1="19.59"
+      y2="19.59"
+    >
+      <stop offset="0" stop-color="#686868" stop-opacity="0" />
+      <stop offset=".16" stop-color="#686868" />
+      <stop offset=".84" stop-color="#686868" />
+      <stop offset="1" stop-color="#686868" stop-opacity="0" />
+    </linearGradient>
+    <g fill="none">
+      <path
+        d="m.5 39.1 149 34.6"
+        stroke="url(#a)"
+        stroke-miterlimit="10"
+        stroke-width="4"
+      />
+      <path
+        d="m101.5 73.6 22-17"
+        stroke="#686868"
+        stroke-miterlimit="10"
+        stroke-width="4"
+      />
+      <path
+        d="m25.1 55.7 22.1-17"
+        stroke="#686868"
+        stroke-miterlimit="10"
+        stroke-width="4"
+      />
+      <path
+        d="m36.2 46.7c0-12 5.3-12.4 10.5-14.2 5-1.7 17.3 1.9 22.6 1.7s8-5 8.4-11.8c-.4 6.9 3.4 13.8 10 16.3 8 3 17.9 2.7 23.2 7.3 7 6 3.3 12.2 1.6 19.1"
+        stroke="#808080"
+        stroke-dasharray="2 4"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="3"
+      />
+    </g>
+  </svg>
+);
+
+export const StepBackIconTag = (
+  <svg
+    id="e0a72a13-c21e-4c58-9a5e-a0ae842f6ad0"
+    data-name="Layer 1"
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    viewBox="0 0 13.6 13.6"
+  >
+    <g style={{ isolation: 'isolate' }}>
+      <path
+        d="M8.7,9.7V9.2L9.6,9a.9.9,0,0,0,.3-.7h.5V13H9.8V9.7Z"
+        transform="translate(-3.2 -3.2)"
+        fill="#d8d8d8"
+      />
+    </g>
+    <path
+      d="M16.4,7.7A6.8,6.8,0,0,0,7.7,3.6l-1.2.6-.4-1L4.8,6.3l3.4-.4L7,4.9l1-.4a5.9,5.9,0,1,1-3.6,7.6,5.7,5.7,0,0,1-.3-2.6H3.2a5.5,5.5,0,0,0,.4,2.8,6.8,6.8,0,0,0,8.7,4.1A6.8,6.8,0,0,0,16.4,7.7Z"
+      transform="translate(-3.2 -3.2)"
+      fill="#d8d8d8"
+    />
+  </svg>
+);
+
+export const StepForwardIconTag = (
+  <svg
+    id="f83b72a1-eb1e-4a2e-b2cf-865592f9a030"
+    data-name="Layer 1"
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    viewBox="0 0 13.6 13.7"
+  >
+    <g style={{ isolation: 'isolate' }}>
+      <path
+        d="M8.9,9.7V9.2L9.8,9a.9.9,0,0,0,.3-.7h.5V13H10V9.7Z"
+        transform="translate(-3.2 -3.1)"
+        fill="#d8d8d8"
+      />
+    </g>
+    <path
+      d="M3.6,7.7a6.8,6.8,0,0,1,8.7-4.1l1.2.6.4-1.1,1.2,3.1-3.4-.4,1.2-1L12,4.4A5.9,5.9,0,1,0,15.6,12a5.4,5.4,0,0,0,.3-2.5h.9a5.5,5.5,0,0,1-.4,2.8,6.8,6.8,0,0,1-8.7,4.1A6.8,6.8,0,0,1,3.6,7.7Z"
+      transform="translate(-3.2 -3.1)"
+      fill="#d8d8d8"
+    />
+  </svg>
+);
--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import miniSVGDataURI from 'mini-svg-data-uri';
 import {
   PlusOutlined,
   MinusOutlined,
@@ -8,27 +7,19 @@ import {
   CaretRightOutlined,
   PauseOutlined,
 } from '@ant-design/icons';
-import scaleBarSVG from '../../assets/scale-bar.svg';
-import stepBackSVG from '../../assets/step-back.svg';
-import stepForwardSVG from '../../assets/step-forward.svg';
-
-const createImageElementFromSVG = (
-  svg: string,
-  style?: React.CSSProperties
-) => {
-  const uri = miniSVGDataURI(svg);
-  return <img src={uri} style={style} />;
-};
+import {
+  ScaleBarIconTag,
+  StepBackIconTag,
+  StepForwardIconTag,
+} from '../assets/svgs';
 
 export const Reset = <HomeOutlined />;
 export const ZoomIn = <PlusOutlined />;
 export const ZoomOut = <MinusOutlined />;
 export const Info = <InfoCircleFilled />;
-export const ScaleBarIcon = createImageElementFromSVG(scaleBarSVG, {
-  width: '60px',
-});
-export const FrameBack = createImageElementFromSVG(stepBackSVG);
-export const FrameForward = createImageElementFromSVG(stepForwardSVG);
+export const ScaleBarIcon = ScaleBarIconTag;
+export const FrameBack = StepBackIconTag;
+export const FrameForward = StepForwardIconTag;
 export const Pause = <PauseOutlined />;
 export const Play = <CaretRightOutlined />;
 
