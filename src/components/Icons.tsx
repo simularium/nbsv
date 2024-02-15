4bd2757187dfbcaba99181d6b55137c9e8d97111
Joe Heffernan <interim17@gmail.com>
2024-02-14 17:18:34 -0800

    add svgs and decalre icons for frame forward and frame back

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -4,14 +4,24 @@ import {
   MinusOutlined,
   HomeOutlined,
   InfoCircleFilled,
+  CaretRightOutlined,
+  PauseOutlined,
 } from '@ant-design/icons';
-import { ScaleBarIconTag } from '../assets/svgs';
+import {
+  ScaleBarIconTag,
+  FrameBackIconTag,
+  FrameForwardIconTag,
+} from '../assets/svgs';
 
 export const Reset = <HomeOutlined />;
 export const ZoomIn = <PlusOutlined />;
 export const ZoomOut = <MinusOutlined />;
 export const Info = <InfoCircleFilled />;
 export const ScaleBarIcon = ScaleBarIconTag;
+export const FrameBack = FrameBackIconTag;
+export const FrameForward = FrameForwardIconTag;
+export const Pause = <PauseOutlined />;
+export const Play = <CaretRightOutlined />;
 
 export default {
   Reset,
