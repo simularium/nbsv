8e819ff2d5fbe61d1a6a979730fb783ae096f38b
Joe Heffernan <interim17@gmail.com>
2024-02-28 13:54:10 -0800

    remove unused icons

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -6,32 +6,22 @@ import {
   InfoCircleFilled,
   CaretRightOutlined,
   PauseOutlined,
-  CaretRightFilled,
-  CaretDownFilled,
 } from '@ant-design/icons';
 import {
   ScaleBarIconTag,
   FrameBackIconTag,
   FrameForwardIconTag,
-  HighlightStarTag,
-  IndeterminateHighlightStarTag,
-  NoHighlightStarTag,
 } from '../assets/svgs';
 
 export const Reset = <HomeOutlined />;
 export const ZoomIn = <PlusOutlined />;
 export const ZoomOut = <MinusOutlined />;
 export const Info = <InfoCircleFilled />;
-export const CaretRight = <CaretRightFilled />;
-export const CaretDown = <CaretDownFilled />;
 export const Pause = <PauseOutlined />;
 export const Play = <CaretRightOutlined />;
 export const ScaleBarIcon = ScaleBarIconTag;
 export const FrameBack = FrameBackIconTag;
 export const FrameForward = FrameForwardIconTag;
-export const IndeterminateHighlightStar = IndeterminateHighlightStarTag;
-export const HighlightStar = HighlightStarTag;
-export const NoHighlightStar = NoHighlightStarTag;
 
 export default {
   Reset,
