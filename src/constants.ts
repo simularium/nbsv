077bd9e0a440cb5944cb78f48065882803c16a60
Joe Heffernan <interim17@gmail.com>
2024-04-15 10:10:49 -0700

    add comment to explain use of visibility seleciton map

--- src/constants.ts
+++ src/constants.ts
@@ -24,6 +24,16 @@ export const SIDE_PANEL_WIDTH: number = 280;
 export const MIN_WIDTH_TO_SHOW_SIDE_PANEL: number = 580;
 export const VIEWER_INITIAL_WIDTH: number = 500;
 export const VIEWER_HEIGHT: number = 580;
+
+/**
+ * The VisibilitySelectionMap holds selection information from which we can derive the state of the agent
+ * checkboxes and of the SelectionStateInfo. The keys are agent names and the array contains possible selections.
+ * The VisibilitySelectionMap is generalied to be used for both hiding and highlighting selections.
+ * if visMap[agentName] = [agentName] // the agent is not selected (default), and by extension, no display states are selected
+ * if visMap[agentName] = [ ] // the agent is selected, and by extension all display states are selected
+ * if visMap[agentName] = [ displayStateName1, displayStateName2... ] // some but not all display states are selected, the agent checkbox is by definition indeterminate
+ * if a selection array would contain all the display state names, it should be changed to an empty array
+ */
 export interface VisibilitySelectionMap {
   [key: string]: string[];
 }
